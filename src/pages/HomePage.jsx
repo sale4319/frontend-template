adbf45ee4ccde7e992bbeb19d94f65e7a0b08819
alestojanovic <aleksandar.stojanovic@productdock.com>
2022-10-25 14:25:54 +0200

    feat: updated homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,5 +4,19 @@ import { RoutingContext } from "../providers/RoutingContext";
 
 export const HomePage = () => {
   const { routeOverview } = useContext(RoutingContext);
-  return <ActionButton onClick={routeOverview} label="Go to overview" />;
+  return (
+    <>
+      <h1 className="gradient-text">
+        They always tell me: <br />
+        "Wow you have such a nice home page!
+        <br />
+        Great homepage, it is very useful!
+        <br /> Your app would now work without it!"
+        <br />
+        I am well aware of these facts, but anyway...
+        <br /> Here is a button that you can click to go to overview...
+      </h1>
+      <ActionButton onClick={routeOverview} label="Go to overview" />
+    </>
+  );
 };
